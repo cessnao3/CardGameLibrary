4b9b518fb0a03b4265a935664961bcd9116d779b
Ian O'Rourke <cessna.ian@gmail.com>
2020-04-18 09:41:33 -0500

    Fixes for better client response

--- GameParameters/Action.cs
+++ GameParameters/Action.cs
@@ -13,12 +13,31 @@ namespace CardGameLibrary.GameParameters
         /// The data parameter to be used in selecting the 
         /// </summary>
         public int data { get; protected set; }
+
+        /// <summary>
+        /// The name associated with the action parameter
+        /// </summary>
         public string name { get; protected set; }
 
+        /// <summary>
+        /// Creates an action parameter
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
         public Action(string name, int data)
         {
             this.data = data;
             this.name = name;
         }
+
+        /// <summary>
+        /// Defines the overall action database to be used for parsing card actions
+        /// </summary>
+        public static readonly Dictionary<int, Action> action_database = new Dictionary<int, Action>()
+        {
+            [EuchreParameters.go_alone.data] = EuchreParameters.go_alone,
+            [EuchreParameters.pickup_card.data] = EuchreParameters.pickup_card,
+            [EuchreParameters.skip.data] = EuchreParameters.skip,
+        };
     }
 }
